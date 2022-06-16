069d6192c9ee5cec80cb2a7b9426c36d14ff512d
Robert Servado <servadorobertgmail.com>
2022-06-15 23:23:03 -0400

    Update play.js by separating the connect function and place it in a another file, client.js

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-//establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  //interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  //Added from 'Connect to the Server' Section
-  conn.on("connect", () => {
-    console.log("you ded cuz you idled");
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting...");
 
