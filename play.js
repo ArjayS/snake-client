07ea6a8b4f89d596713bd4fe8f7cab4146e589a6
Robert Servado <servadorobertgmail.com>
2022-06-15 22:50:41 -0400

    Added play.js

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+//establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  //interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  //Added from 'Connect to the Server' Section
+  conn.on("connect", () => {
+    console.log("you ded cuz you idled");
+  });
+
+  return conn;
+};
+
+console.log("Connecting...");
+
+connect();
