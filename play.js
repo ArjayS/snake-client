c5e5cb3a39bb43385ce51d277df3697a48120fe0
Robert Servado <servadorobertgmail.com>
2022-06-19 19:38:41 -0400

    Added the stdin for terminating the game using ctrl + c

--- play.js
+++ play.js
@@ -1,5 +1,44 @@
 const { connect } = require("./client");
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  //Added on Stdin Setup section
+  stdin.on("data", handleUserInput);
+
+  const handleUserInput = function (key) {
+    // your code here
+    if (key === "\u0003") {
+      process.exit();
+    }
+  };
+
+  return stdin;
+};
+
 console.log("Connecting...");
 
-connect();
+connect(setupInput);
+
+// const stdin = process.stdin;
+// // don't worry about these next two lines of setup work.
+// stdin.setRawMode(true);
+// stdin.setEncoding("utf8");
+
+// ////////////
+// // Event Handling for User Input
+// ////////////
+
+// // on any input from stdin (standard input), output a "." to stdout
+// stdin.on("data", (key) => {
+//   process.stdout.write(".");
+//   if (key === "\u0003") {
+//     process.exit();
+//   }
+// });
+
+// console.log("after callback");
