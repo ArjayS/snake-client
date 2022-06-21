1c30227359a463dbccbfa23e53b19437f8002d57
Robert Servado <servadorobertgmail.com>
2022-06-20 20:59:04 -0400

    Modified input.js so that it contains a global variable connection and that the value of that is established to be from thte connect return value

--- input.js
+++ input.js
@@ -1,5 +1,12 @@
+const { connect } = require("./client");
+
+let connection; // We establish a global variable for the other function to use
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = (conn) => {
+  // We placed a paramenter called conn within the setupInput function
+  connection = conn; //We created a copy of conn value to the connection global variable. Now the global variable connection has now the same value as conn.
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,8 +20,25 @@ const setupInput = function () {
 
 const handleUserInput = function (key) {
   // your code here
+
   if (key === "\u0003") {
     process.exit();
+  }
+  if (key === "w") {
+    // console.log(`w = "Move: up"`);
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    // console.log(`a = "Move: left"`);
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    // console.log(`s = "Move: down"`);
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    // console.log(`d = "Move: right"`);
+    connection.write("Move: right");
   }
 };
 
