584dee3c40588aadf9a29aefe63bd0a5569b1a4a
Robert Servado <servadorobertgmail.com>
2022-06-21 18:49:14 -0400

    Add constants.js to hold all our necessary constants used

---
+++ constants.js
@@ -0,0 +1,23 @@
+const IP = "localhost";
+
+const PORT = 50541;
+
+let playerName = "Name: RCS";
+
+const move_up_key = "Move: up";
+
+const move_left_key = "Move: left";
+
+const move_down_key = "Move: down";
+
+const move_right_key = "Move: right";
+
+module.exports = {
+  IP,
+  PORT,
+  playerName,
+  move_up_key,
+  move_left_key,
+  move_down_key,
+  move_right_key,
+};
