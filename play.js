ae71bfadf7a6c03f480b8d3392450e03c6ae8d77
Robert Servado <servadorobertgmail.com>
2022-06-21 22:15:09 -0400

    Clean the code for final submission

--- play.js
+++ play.js
@@ -1,27 +1,8 @@
 const { connect } = require("./client");
 const { setupInput } = require("./input");
 
-console.log("Connecting...");
+console.log("connected...");
 
-const connection = connect(); //Will return the conn value from client.js therefore we need to store the value in a variable
+const connection = connect();
 
-setupInput(connection); //We are using the connection variable that we establish a value from client.js and using it in the setupInput function
-
-// const stdin = process.stdin;
-// // don't worry about these next two lines of setup work.
-// stdin.setRawMode(true);
-// stdin.setEncoding("utf8");
-
-// ////////////
-// // Event Handling for User Input
-// ////////////
-
-// // on any input from stdin (standard input), output a "." to stdout
-// stdin.on("data", (key) => {
-//   process.stdout.write(".");
-//   if (key === "\u0003") {
-//     process.exit();
-//   }
-// });
-
-// console.log("after callback");
+setupInput(connection);
