569c636070fb58d78152f696a2f74dd796294458
Robert Servado <servadorobertgmail.com>
2022-06-21 22:15:50 -0400

    Clean the code for final submission

--- client.js
+++ client.js
@@ -11,33 +11,27 @@ const connect = function () {
   //interpret incoming data as text
   conn.setEncoding("utf8");
 
-  // //Added from 'Connect to the Server' Section
-  // conn.on("connect", () => {
-  //   console.log("you ded cuz you idled");
-  // });
-
-  // //Added from 'Snake Initials' Section
-  // conn.on("connect", () => {
-  //   console.log("Successfully connected to game server");
-  // });
-
-  //Added from 'Snake Initials' Section
   conn.on("connect", () => {
     conn.write(playerName);
 
     setTimeout(() => {
+      //Canned Messages
       conn.write("Say: sssss");
     }, 5000);
     setTimeout(() => {
+      //Canned Messages
       conn.write("Say: sssss apple");
     }, 3000);
     setTimeout(() => {
+      //Canned Messages
       conn.write("Say: hungry");
     }, 10000);
     setTimeout(() => {
+      //Canned Messages
       conn.write("Say: yum apple ssss");
     }, 15000);
     setTimeout(() => {
+      //Canned Messages
       conn.write("Say: ssssssssssssss");
     }, 20000);
 
@@ -69,9 +63,5 @@ const connect = function () {
 
   return conn;
 };
-
-// process.stdin.on("data", (typedServerMessage) => {
-//   client.write(typedServerMessage);
-// });
 
 module.exports = { connect };
